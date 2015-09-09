4db007633bf8dfce999326dadf8a8bc35f031ae6
Rens Admiraal <rens@simplyadmire.com>
2015-09-09 16:32:24 +0200

    [TASK] Kickstart ObjectModel

---
+++ src/Object/Domain/Model/ObjectModel.php
@@ -0,0 +1,31 @@
+<?php
+namespace SimplyAdmire\Zaaksysteem\Object\Domain\Model;
+
+final class ObjectModel
+{
+
+    private $actions;
+
+    private $dateCreated;
+
+    private $dateModified;
+
+    private $id;
+
+    private $label;
+
+    private $relatedTypes;
+
+    private $securityRules;
+
+    private $type;
+
+    private $values;
+
+    public function __construct(array $data)
+    {
+
+    }
+
+
+}
